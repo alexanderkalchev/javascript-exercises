e58a261d42006269ce137cd0252034e8486cebcf
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-07 12:21:29 +0200

    Finish exercise foundations/09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(typeof start !== "number" || typeof end !== "number")return "ERROR";
+    if(start < 0 || end < 0) return "ERROR";
+    if(Math.floor(start) !== start || Math.floor(end) !== end) return "ERROR";
 
+    if(end < start){
+        start += end; // 14 + 13
+        end = start - end; // 27 - 13 = 14
+        start -= end; // 27 - 14 = 13
+    }
+
+    console.log("Start: " + start);
+    console.log("End: " + end);
+
+    let sum = 0;
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
