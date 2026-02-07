26406091b7e10946c9083d8ca105508d87b11001
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-07 12:06:59 +0200

    Finish exercise foundations/08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const banned = Array.from(arguments).slice(1);
+    for (let i = 0; i < arr.length; i++) {
+        if(banned.includes(arr[i])){
+            arr.splice(i, 1)
+            i--;
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
