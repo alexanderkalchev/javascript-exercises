4f38276e0cd399888779a7175d67668a8ad5dd8a
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-12 18:00:01 +0200

    Finish exercise foundations/13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.toLowerCase().replaceAll(" ", "").replaceAll(",","").replaceAll(".", "").replaceAll("!","");
+    let reverse = word.split("").reverse().join("");
+    return reverse === word;
 };
 
 // Do not edit below this line
