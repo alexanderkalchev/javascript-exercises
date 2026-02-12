8b9203c9cac946f54b3a28f44029be28834006b4
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-12 19:15:33 +0200

    Finish exercise foundations/16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if(!oldest["yearOfDeath"]) oldest["yearOfDeath"] = (new Date()).getFullYear();
+        if(!person["yearOfDeath"]) person["yearOfDeath"] = (new Date()).getFullYear();
+        console.log(oldest);
+        console.log(person);
+        let currAge = person["yearOfDeath"] - person["yearOfBirth"];
+        let oldAge = oldest["yearOfDeath"] - oldest["yearOfBirth"];
 
+        // if(currAge > oldAge) return person;
+        // else return oldest;
+        return currAge > oldAge ? person : oldest
+    })
 };
 
 // Do not edit below this line
