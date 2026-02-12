7aa682bb0e37e529246cc032d76c05a2a3b96a61
sashkoPogramista <alexanderkkalchev@gmail.com>
2026-02-12 18:11:47 +0200

    Finish exercise foundations/14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if(num < 0) return "OOPS";
+    if(num === 0) return 0;
+
+    first = 1;
+    second = 1;
+    counter = 2;
+    while(counter < num){
+        second += first;
+        first = second - first;
+        counter++;
+    }
+    return second;
 };
 
 // Do not edit below this line
